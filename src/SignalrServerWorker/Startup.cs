e07244f751d707495cd71bea4f09a1b9531d86ef
João Jesus <joao29a@gmail.com>
2020-09-09 17:42:22 -0300

    Adding chat as example to use with docker

---
+++ src/SignalrServerWorker/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalrServerWorker.Hubs;
+
+namespace SignalrServerWorker
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR()
+                .AddStackExchangeRedis("localhost:9090");
+
+            services.AddHostedService<Worker>();
+
+            services.AddSingleton<IUserIdProvider, TechUserIdProvider>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<TechTalkChatHub>("/hubs/chat");
+            });
+        }
+    }
+}
